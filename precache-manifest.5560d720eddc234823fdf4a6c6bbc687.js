d6ba1cc6dfc639b2304acaacf99056dd774219db
Scott Cardinali <scott.cardinali@gmail.com>
2023-10-31 15:38:19 -0400

    Updates

---
+++ precache-manifest.5560d720eddc234823fdf4a6c6bbc687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e018da804c504c245f0",
+    "url": "/portfolio/static/css/main.1bebe923.chunk.css"
+  },
+  {
+    "revision": "4e018da804c504c245f0",
+    "url": "/portfolio/static/js/main.2f74ecda.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "51975fba70d8ffc36046",
+    "url": "/portfolio/static/css/2.afcdded7.chunk.css"
+  },
+  {
+    "revision": "51975fba70d8ffc36046",
+    "url": "/portfolio/static/js/2.cf7ffb7e.chunk.js"
+  },
+  {
+    "revision": "9b6356269430af799e7124f4e8d6646d",
+    "url": "/portfolio/static/media/10-7.9b635626.png"
+  },
+  {
+    "revision": "1ce89ac49e9234677ddee543bc8e406a",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a206acd33a23ce403c76caba28b098b6.js"
+  "/portfolio/precache-manifest.5560d720eddc234823fdf4a6c6bbc687.js"
 );
 
 workbox.clientsClaim();
